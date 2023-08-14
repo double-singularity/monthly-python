ebf7f7f5bd367a85a2dacad51c3225a7bb297d51
double-singularity <rstu1081@gmail.com>
2023-08-14 22:43:45 +0700

    Add framerate control

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption('Alien Invasion')
 
+        self.clock = pygame.time.Clock()
+
     def run_game(self) -> None:
         """Start the game loop"""
         
@@ -20,6 +22,7 @@ class AlienInvasion:
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
+        self.clock.tick(60)
 
 if __name__ == '__main__':
     # Create the game instance
