cc8c372a05efe6a99c7f4c811fc94c9b08c70c9a
double-singularity <rstu1081@gmail.com>
2023-08-14 22:47:22 +0700

    Fixing identation and add screen fill

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,7 @@ class AlienInvasion:
         pygame.display.set_caption('Alien Invasion')
 
         self.clock = pygame.time.Clock()
+        self.bg_color = (230, 230, 230)
 
     def run_game(self) -> None:
         """Start the game loop"""
@@ -20,9 +21,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
-        self.clock.tick(60)
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Create the game instance
